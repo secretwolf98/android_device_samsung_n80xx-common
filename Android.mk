50bfa5646a48247a2cfbe2411020f8c1a7c43983
Secretwolf98 <jordanluders@gmail.com>
2020-01-30 15:54:49 -0600

    Fix compile errors for the n8020.

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter n8000 n8010 n8013 n8020,$(TARGET_DEVICE)),)
+ifneq ($(filter n8000 n8010 n8013,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
